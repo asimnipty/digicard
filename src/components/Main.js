49c8d14d2c67510110654ca4d76ce642fbc7deb4
Asim <asimnipty@yahoo.com>
2022-06-17 01:22:49 +0600

    commit img

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,8 @@ export default function Main(){
             <h3 className="mid-name"> Asim K Nipty </h3>
             <h4 className="pos"> Learner </h4>
             <h4 className="email-text"> asimnipty@yahoo.com </h4>
-            <button type = "button" className="email-btn"><img src="./icon/email.png"/> Email </button>
-            <button type = "button" className="linkdn-btn"> <img src="./icon/lnk.png"/> LinkedIn </button>
+            <button type = "button" className="email-btn"><img src= {require('../icon/email.png')}/> Email </button>
+            <button type = "button" className="linkdn-btn"> <img src= {require('../icon/lnk.png')}/> LinkedIn </button>
             <h3 className="about"> About </h3>
             <p className="about-text"> I am a learner with particular interest in making things simple and automating daily tasks. I try to keep up with best practices, and am always looking for new things to learn. </p>
             <h3 className="interest"> Interests  </h3>
