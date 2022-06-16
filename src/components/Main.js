542ec5d5b0e986038ec2183c8b756977c249481d
Asim <asimnipty@yahoo.com>
2022-06-16 19:06:30 +0600

    commit

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Main(){
+    return (
+        <div className="mid">
+            <h3 className="mid-name"> Asim K Nipty </h3>
+            <h4 className="pos"> Learner </h4>
+            <h4 className="email-text"> asimnipty@yahoo.com </h4>
+            <button type = "button" className="email-btn"><img src="./icon/email.png"/> Email </button>
+            <button type = "button" className="linkdn-btn"> <img src="./icon/lnk.png"/> LinkedIn </button>
+            <h3 className="about"> About </h3>
+            <p className="about-text"> I am a learner with particular interest in making things simple and automating daily tasks. I try to keep up with best practices, and am always looking for new things to learn. </p>
+            <h3 className="interest"> Interests  </h3>
+            <p className="about-text">  Movie . Music lover. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. culture. </p>
+                             
+        </div>
+    )
+    
+}
