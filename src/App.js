e0106b3f5e88418b99c7580aab006bd5f8a86d48
Asim <asimnipty@yahoo.com>
2022-06-16 19:12:40 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Top from "./components/Top"
 import Main from "./components/Main"
 import Bottom from "./components/Bottom"
+import "./style.css"
 
 export default function App(){
     return (
--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -3,7 +3,7 @@ import React from "react"
  export default function Bottom(){
      return(
          <div className="bottom">
-            <img src="../icon/twitter.png" className = "tw"/ >
+            <img src="../icon/twitter.png" className = "tw"/>
             <img src="../icon/fb.png" className = "fb"/>
             <img src="../icon/git.png" className = "git"/>
             <img src="../icon/ig.png" className = "ig"/>
